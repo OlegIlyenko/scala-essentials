9a451fe7eab690661a896c4f3b1da916af118037
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2010-02-18 00:45:37 +0100

    simplified CommandLine

--- src/main/scala/org/angelsmasterpiece/scala/essentials/option/Argument.scala
+++ src/main/scala/org/angelsmasterpiece/scala/essentials/option/Argument.scala
@@ -11,46 +11,81 @@ import org.angelsmasterpiece.scala.essentials.convert.{AggregatingConverter, Def
 
 trait CommandLine {
 
+    val longPrefix = "--"
+    val shortPrefix = "-"
+
     protected val converter = DefaultConverter
 
     def parse(source: Array[String]): Option[List[ValidationError]] = {
         val errors = new ListBuffer[ValidationError]
-        val definedArgs = arguments
 
-        var currentArg: Option[Argument[_]] = None
-
-        source.foreach { arg =>
-            val findFn = if (arg startsWith CommandLine.LongPrefix)
-                Some((a: Argument[_]) => (CommandLine.LongPrefix + a.longName) == arg)
-            else if (arg startsWith CommandLine.ShortPrefix)
-                Some((a: Argument[_]) => (CommandLine.ShortPrefix + a.shortName) == arg)
-            else
-                None
-
-            findFn match {
-                case Some(fn) => definedArgs.find(fn) match {
-                    case None => errors += ValidationError(None, "Undefined argument " + arg)
-                    case Some(a) => if (a.parameter) currentArg = Some(a) else a.asInstanceOf[Argument[Boolean]].value = Some(true)
-                }
-                case None => currentArg match {
-                    case Some(a) =>
-                        a.setValue(arg, converter) match {
-                            case Some(err) => errors ++= err
-                            case None =>
-                        }
-                        currentArg = None
-                    case None => errors += ValidationError(None, "Strange parameter '" + arg + "'. It does not belongs to any argument")
-                }
-            }
-        }
-
-        definedArgs.filter(a => !a.present && !a.optional && !errors.exists(e => e.argument.isDefined && e.argument.get == a))
-            .foreach(a => errors += ValidationError(Some(a), "Argument not set!"))
-        
-        definedArgs.filter(a => !a.present && !a.parameter).foreach(_.asInstanceOf[Argument[Boolean]].value = Some(false))
+        parseArguments(source, errors)
+        validateAbsentArguments(errors)
+        falseAbsentBooleanArguments()
 
         if (errors.isEmpty) None else Some(errors.toList)
     }
+
+    private def parseArguments(source: Array[String], errors: ListBuffer[ValidationError]) = {
+        var currentArg: Option[Argument[_]] = None
+
+        source.foreach { inputArgument =>
+            currentArg = getArgumentFindFunction(inputArgument) match {
+                case Some(findFunction) => findArgument(findFunction, inputArgument, currentArg, errors)
+                case None => findArgumentParameter(inputArgument, currentArg, errors)
+            }
+        }
+    }
+
+    private def getArgumentFindFunction(argument: String) =
+        if (arguments startsWith longPrefix)
+            Some((a: Argument[_]) => (longPrefix + a.longName) == argument)
+        else if (arguments startsWith shortPrefix)
+            Some((a: Argument[_]) => (shortPrefix + a.shortName) == argument)
+        else
+            None
+
+    private def findArgument(findFunction: Argument[_] => Boolean,
+                             inputArgument: String,
+                             currentArg: Option[Argument[_]],
+                             errors: ListBuffer[ValidationError]) =
+        arguments.find(findFunction) match {
+            case None =>
+                errors += ValidationError(None, "Undefined argument " + inputArgument)
+                currentArg
+            case Some(a) =>
+                if (a.parameter)
+                    Some(a)
+                else {
+                    a.asInstanceOf[Argument[Boolean]].value = Some(true)
+                    None
+                }
+        }
+
+    private def findArgumentParameter(inputArgument: String,
+                                      currentArg: Option[Argument[_]], 
+                                      errors: ListBuffer[ValidationError]) =
+        currentArg match {
+            case Some(a) =>
+                setArgumentParameter(inputArgument, a, errors)
+                None
+            case None =>
+                errors += ValidationError(None, "Strange parameter '" + inputArgument + "'. It does not belongs to any argument")
+                currentArg
+        }
+
+    private  def setArgumentParameter(inputArgument: String, argument: Argument[_], errors: ListBuffer[ValidationError]) =
+        argument.setValue(inputArgument, converter) match {
+            case Some(err) => errors ++= err
+            case None =>
+        }
+
+    private def validateAbsentArguments(errors: ListBuffer[ValidationError]) =
+        arguments.filter(a => !a.present && !a.optional && !errors.exists(e => e.argument.isDefined && e.argument.get == a))
+            .foreach(a => errors += ValidationError(Some(a), "Argument not set!"))
+
+    private def falseAbsentBooleanArguments() =
+        arguments.filter(a => !a.present && !a.parameter).foreach(_.asInstanceOf[Argument[Boolean]].value = Some(false))
 
     def parseOrExit(source: Array[String]): Unit = (parse(source): @unchecked) match {
         case Some(errors) =>
@@ -61,15 +96,19 @@ trait CommandLine {
          case None =>
     }
 
-    def usage: String = "Usage:\n application.executable [ARGUMENTS]\n\nArguments:\n" + arguments
-        .map(a =>
-                " \t" + CommandLine.LongPrefix + a.longName +
-                (if (a.shortName != null) ", " + CommandLine.ShortPrefix + a.shortName else "") +
-                (if (a.parameter) " [" + (if (a.parameterName.isDefined) a.parameterName.get else a.valueType.getName) +  "]" else "") +
-                (if(!a.optional) " (required)" else "") +
-                "\n\t\t" +
-                (if (a.description != null) a.description + "\n" else ""))
-        .mkString("\n")
+    def usage: String = usageTitle + usageBody
+
+    protected def usageTitle = "Usage:\n application.executable [ARGUMENTS]\n\nArguments:\n"
+
+    protected def usageBody = arguments.map(getArgumentUsage).mkString("\n")
+
+    protected def getArgumentUsage(a: Argument[_]) =
+        " \t" + longPrefix + a.longName +
+        (if (a.shortName != null) ", " + shortPrefix + a.shortName else "") +
+        (if (a.parameter) " [" + (if (a.parameterName.isDefined) a.parameterName.get else a.valueType.getName) +  "]" else "") +
+        (if(!a.optional) " (required)" else "") +
+        "\n\t\t" +
+        (if (a.description != null) a.description + "\n" else "")
 
     def apply(name: String): Option[_] = arguments.find(a => (a.alias.isDefined && a.alias.get == name) || a.longName == name) match {
         case Some(a) => a.value
@@ -78,13 +117,9 @@ trait CommandLine {
 
     def arguments: List[Argument[_]] = this.getClass.getMethods
         .filter(m => classOf[Argument[_]].isAssignableFrom(m.getReturnType))
-        .map(m => m.invoke(this).asInstanceOf[Argument[_]]).sortBy(_.longName).toList
-
-}
-
-object CommandLine {
-    val LongPrefix = "--"
-    val ShortPrefix = "-"
+        .map(m => m.invoke(this).asInstanceOf[Argument[_]])
+        .sortBy(_.longName)
+        .toList
 }
 
 case class Argument[T](
@@ -109,24 +144,28 @@ case class Argument[T](
         val errors = new ListBuffer[ValidationError]
         
         converter.convert[String , T](arg) match {
-            case Some(result) => v = validator match {
-                case Some(validate) => validate(this, result) match {
-                    case Some(err) =>
-                        errors ++= err
-                        None    
+            case Some(result) =>
+                v = validate(result) match {
+                    case Some(err) => errors ++= err; None
                     case None => Some(result)
                 }
-                case None => Some(result)
-            }
             case None => errors += ValidationError(Some(this), "Unable to convert parameter '" + arg + "' of type " + valueType.getName)
         }
 
         if (errors.isEmpty) None else Some(errors.toList)
     }
 
+    private def validate(newValue: T) = validator match {
+        case Some(validate) => validate(this, newValue) match {
+            case Some(err) => Some(err)
+            case None => None
+        }
+        case None => None
+    }
+
     def apply(): T = value match {
         case Some(v) => v
-        case None => throw new IllegalStateException("Value for the argument " + CommandLine.LongPrefix + longName + " is not set!")
+        case None => throw new IllegalStateException("Value for the argument '" + longName + "' is not set!")
     }
 
     def present = value.isDefined
@@ -139,7 +178,7 @@ object FlagArgument {
 
 case class ValidationError(val argument: Option[Argument[_]], val errorDescription: String) {
     override def toString = "Problem with argument" + (argument match {
-        case Some(arg) => " " + CommandLine.LongPrefix + arg.longName
+        case Some(arg) => " '" + arg.longName + "'"
         case None => ""
     })  + ": " + errorDescription
 }
